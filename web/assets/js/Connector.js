7b1fa3b5ca63c0124b9effc526eacf8219bb4518
fei <fei@fei-Ubuntu>
2018-09-26 22:55:52 +0800

    0926_登录页面的JS+CSS控制文件 Signed-off-by:fei <fei@fei-Ubuntu>

---
+++ web/assets/js/Connector.js
@@ -0,0 +1,34 @@
+/**
+ * 包装jquery的ajax请求，
+ * @param {type} opts 
+ * {
+ *      url: "",
+ *      params: {},
+ *      success:function(json){},
+ *      failure:function(json){}
+ * }
+ * @returns {void}
+ */
+Connector = function(opts){
+    $.ajax({
+        url:opts.url,
+        data:{
+            request: JSON.stringify(opts.params)
+        },
+        method:"POST", 
+        success:function(data){
+            var json = JSON.parse(data);
+            if(json.success && opts.success && typeof opts.success === "function"){
+                opts.success(json);
+            }else if(!json.success && opts.failure && typeof opts.failure === "function"){
+                opts.failure(json);
+            }else{
+                $.alert(json.message);
+            }
+        },
+        error:function(data){
+            $.alert("网络错误，请检查您的网络连接!");
+        }
+    });
+};
+
